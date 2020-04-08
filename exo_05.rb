a6f4da5d1bc3e5ea39f3362788cfad3ef24b371d
MaxLok0610 <maximelokengo@hotmail.fr>
2020-04-08 15:37:19 +0200

    first commit

---
+++ exo_05.rb
@@ -0,0 +1,22 @@
+puts "On va compter le nombre d'heures de travail à THP"
+puts "Travail : #{10 * 5 * 11}" # 10 = heures par jour, 5= nombre de jours dans la semaine, 11= Nombre de semaine
+puts "En minutes ça fait : #{10 * 5 * 11 * 60}" # Le résultats de toute ses informations en minutes
+
+puts "Et en secondes ?"
+
+puts 10 * 5 * 11 * 60 * 60 #Le résultat en seconde
+
+puts "Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?"
+
+puts 3 + 2 < 5 - 7  
+
+puts "Ça fait combien 3 + 2 ? #{3 + 2}" # Le résultat correspondant aux valeurs
+puts "Ça fait combien 5 - 7 ? #{5 - 7}" # Le résultat correspondant aux valeurs
+
+puts "Ok, c'est faux alors !"
+
+puts "C'est drôle ça, faisons-en plus :"
+
+puts "Est-ce que 5 est plus grand que -2 ? #{5 > -2}"  #Ceci est une bollean car la réponse est soit par true ou false
+puts "Est-ce que 5 est supérieur ou égal à -2 ? #{5 >= -2}" #
+puts "Est-ce que 5 est inférieur ou égal à -2 ? #{5 <= -2}" #

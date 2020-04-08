1c25939210552d9a87e2e835d8205b6580000ea4
MaxLok0610 <maximelokengo@hotmail.fr>
2020-04-08 16:23:58 +0200

    first commit

---
+++ exo_07c.rb
@@ -0,0 +1,7 @@
+user_name = gets.chomp
+puts user_name
+
+=begin La différence c'est que dans le premier on a : Bonjour, c'est quoi ton blaze car il est dans puts "" et un get.chomp à saisir par le consommateur
+le deuxième: même chose suivi de > car il dans print"" et un get chomp aussi
+le troisième : Il n'a rien du tout car ça sera à l'utilisateur de saisir sur le champs
+=end

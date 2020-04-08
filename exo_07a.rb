37746f9d56556bb73fc118893010d36a66a76b17
MaxLok0610 <maximelokengo@hotmail.fr>
2020-04-08 16:09:39 +0200

    first commit

---
+++ exo_07a.rb
@@ -0,0 +1,10 @@
+puts "Bonjour, c'est quoi ton blase ?"
+user_name = gets.chomp
+puts user_name
+
+=begin gets permet de demander une saisie auprès du consommateurs
+ 
+et gets.chomp renvoie une chaîne de caractère. On peut remarquer aussi qu'il n'y a que "Bonjour, c'est quoi ton blaze" qui est marqué 
+
+=end
+

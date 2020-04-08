f77ad614e2b146f192efa1790a8a200646e92f5d
MaxLok0610 <maximelokengo@hotmail.fr>
2020-04-08 16:51:35 +0200

    first commit

---
+++ exo_08.rb
@@ -0,0 +1,10 @@
+puts "Quel est votre prénom ?"
+print ">"
+user_name = gets.chomp
+
+puts "Bonjour, #{user_name} ! "
+
+=begin L'utilisateur peut écrire sa réponse en dessous. 
+ print '>' est indiqué pour lui indiquer où répondre.
+=end
+
